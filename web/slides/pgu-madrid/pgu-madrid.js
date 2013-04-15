6a65895fc8506287ecfbcba9d9c5b8f0da8f6455
Pascal Guilcher <guilcher.p@sfeir.com>
2013-04-15 17:59:40 +0200

    ..

---
+++ web/slides/pgu-madrid/pgu-madrid.js
@@ -0,0 +1,12 @@
+(function() {
+
+    var dom = [
+        , '  <div id="pgu-md-sf" class="pgu-md pgu-md-sf">SF</div>'
+        , '  <div id="pgu-md-e" class="pgu-md pgu-md-e">≡</div>'
+        , '  <div id="pgu-md-ir" class="pgu-md pgu-md-ir">IR</div>'
+    ]
+
+    $('#pgu-madrid').html(dom.join(''));
+    $('#pgu-madrid').addClass('pgu-md-article');
+
+})();
---
+++ web/slides/pgu-nantes/pgu-nantes.js
@@ -0,0 +1,10 @@
+(function() {
+
+    var dom = [
+        , '  <div id="pgu-nt-sf" class="pgu-nt pgu-nt-sf">SF</div>'
+        , '  <div id="pgu-nt-e" class="pgu-nt pgu-nt-e">≡</div>'
+        , '  <div id="pgu-nt-ir" class="pgu-nt pgu-nt-ir">IR</div>'
+    ]
+
+    $('#pgu-nantes').html(dom.join(''));
+})();
