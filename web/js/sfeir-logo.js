f0ed81069dfd66112a49c7fd8cc6c7c892df6a92
Pascal Guilcher <guilcher.p@sfeir.com>
2013-04-16 11:37:41 +0200

    ..

---
+++ web/js/sfeir-logo.js
@@ -0,0 +1,89 @@
+// see http://animateyourhtml5.appspot.com/
+function firstRender(id, callback)
+{
+    // dimensions
+    var canvascontainer = document.getElementById(id);
+    // relaunch if not ready
+    if (canvascontainer == null || canvascontainer.clientWidth == 0 || canvascontainer.clientHeight == 0 || canvascontainer.animation === -1) { window.setTimeout(function() {firstRender(id);}, 200); return; }
+
+    // do nothing if already running
+    if (canvascontainer.animation)
+        return;
+    else
+        canvascontainer.animation = true;
+
+    var width  = canvascontainer.clientWidth;
+    var height = canvascontainer.clientHeight-2; // unfathomable bug: why do I get a scroll bar with the full height ???
+
+    // renderer
+    var renderer = new THREE.WebGLRenderer ({antialias: true});
+    renderer.setSize(width, height);
+
+    // glue to HTML element
+    canvascontainer.appendChild(renderer.domElement);
+    renderer.setClearColorHex(0x000000, 0);
+    renderer.clear();
+
+    // camera
+    var camera = new THREE.PerspectiveCamera(35, width / height, 1, 3000);
+    camera.position.z = 300; // 300
+
+    // scene
+    var scene = new THREE.Scene();
+
+    // lights
+    var light1 = new THREE.DirectionalLight(0xffffff, 1);
+    light1.position.set(1, 1, 0.3);
+    scene.add(light1);
+
+    var light2 = new THREE.DirectionalLight(0xffffff, 1);
+    light2.position.set(-1, 1, -0.3);
+    scene.add(light2);
+
+    var light3 = new THREE.DirectionalLight(0xffffff, 0.7);
+    light3.position.set(0.5, -0.5, 1);
+    scene.add(light3);
+
+    // textured cube
+    callback(scene);
+
+    var t0 = new Date().getTime();
+
+    var anim = function()
+    {
+        var t = new Date().getTime();
+        // when the canvas dies, abort animation loop
+        if (canvascontainer.animation)
+        {
+            scene.traverse(function(object)
+            {
+                // models: slow rotation
+                if (object instanceof THREE.Object3D)
+                {
+                    object.rotation.y = (t-t0)/1000 - Math.PI/2;
+                };
+            } );
+
+            renderer.render(scene, camera);
+            requestAnimationFrame(anim);
+        }
+    };
+    requestAnimationFrame(anim);
+}
+
+function killFirstRender(id)
+{
+    var canvascontainer = document.getElementById(id);
+    canvascontainer.animation = -1; // dying (set this wether the animation is running or not)
+    window.setTimeout(function()
+    {
+        // signal death of this animation
+        delete canvascontainer.animation;
+        if (canvascontainer.firstChild !== null) {
+            //              canvascontainer.removeChild(canvascontainer.firstChild); // [pgu] it does not work
+            canvascontainer.innerHTML = "";
+        }
+
+    }, 300);
+}
+
--- web/slides/pgu-nantes/pgu-nantes.js
+++ web/slides/pgu-nantes/pgu-nantes.js
@@ -2,9 +2,15 @@
 
     // init DOM
     var dom_of_article = [
-        , '  <div id="pgu-nt-sf" class="pgu-nt pgu-nt-sf">SF</div>'
-        , '  <div id="pgu-nt-e" class="pgu-nt pgu-nt-e">≡</div>'
-        , '  <div id="pgu-nt-ir" class="pgu-nt pgu-nt-ir">IR</div>'
+        , '  <div id="pgu-nt-open"       class="pgu-nt-open">[</div>'
+        , '  <div id="pgu-nt-sf"         class="pgu-nt-sf">SF</div>'
+        , '  <div id="pgu-nt-e"          class="pgu-nt-e">≡</div>'
+        , '  <div id="pgu-nt-ir"         class="pgu-nt-ir">I R</div>'
+        , '  <div id="pgu-nt-close"      class="pgu-nt-close">]</div>'
+        , '  <div id="pgu-nt-cube"       class="pgu-nt-cube">'
+        , '    <div id="pgu-nt-cube-container"  class="pgu-nt-cube-container"></div>'
+        , '    <div id="pgu-nt-cube-mask"  class="pgu-nt-cube-mask"></div>'
+        , '  </div>'
     ]
 
     var article = $('#pgu-nantes');
@@ -27,9 +33,60 @@
     window.SLIDES['pgu-nantes'] = {
         'reset': function() {
                 console.log('reset nantes');
+
+            $('#pgu-nt-cube-mask').fadeIn();
+
+            window.killFirstRender('pgu-nt-cube-container');
+            $('#pgu-nt-cube').removeClass('pgu-nt-cube-go-front');
+
+            $('#pgu-nt-e').fadeIn();
+
+            $('#pgu-nt-open').removeClass('pgu-nt-go-to-left');
+            $('#pgu-nt-close').removeClass('pgu-nt-go-to-right');
+
         }
       , 'execute': function() {
                 console.log('execute nantes');
+
+            $('#pgu-nt-e').off('click').on('click', function () {
+                $('#pgu-nt-open').addClass('pgu-nt-go-to-left');
+                $('#pgu-nt-close').addClass('pgu-nt-go-to-right');
+
+                $('#pgu-nt-e').delay(2700).fadeOut('slow',
+                    function() {
+                        // avance mask & container
+                        $('#pgu-nt-cube').addClass('pgu-nt-cube-go-front');
+
+                        $('#pgu-nt-cube-mask').delay(2700).fadeOut('slow');
+
+                        var callback = function(scene) {
+                            var loader = new THREE.ColladaLoader();
+                            loader.options.convertUpAxis= true;
+                            loader.options.upAxis = 'Y';
+
+                            loader.load('/slides/pgu-nantes/pgu-cube.dae',
+                                function(collada)
+                                {
+                                    var scale = new THREE.Vector3(1,1,1);
+                                    var model = collada.scene;
+                                    model.position.z = 0;
+                                    model.position.y = -20;
+                                    model.scale.copy(scale);
+                                    model.name = "pgu-nt-cube";
+
+                                    // window.model_E = model;
+
+                                    scene.add(model);
+                                } );
+                        }
+                        // render cube
+                        window.firstRender('pgu-nt-cube-container', callback);
+                    }
+                );
+
+            });
+
+
         }
     };
 
