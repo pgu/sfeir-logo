1a132457b526c75692567b43a6b87acd6f167437
Pascal Guilcher <guilcher.p@sfeir.com>
2013-04-17 18:57:52 +0200

    ..

--- web/slides/pgu-new-york/pgu-new-york.js
+++ web/slides/pgu-new-york/pgu-new-york.js
@@ -36,19 +36,63 @@
     window.pgu_new_york = function() {
         var is_on = false;
 
-        var selected_axis = 'y';
+        var selected_axis = 'z';
         var coeff_dir = 1;
 
         var interval_id_for_animation = null;
         var the_cube = null;
+
+        var z_max = 250;
+        var z_min = -500;
+        var x_max_coef = -0.42; // parseFloat((-80/190).toFixed(2));
+        var x_min_coef = 0.42; // parseFloat((79/190).toFixed(2));
+        var y_max_coef = -0.305; // parseFloat((-58/190).toFixed(2));
+        var y_min_coef = 0.335; // parseFloat((58/190).toFixed(2));
+
+        var is_changing = false;
+
+        var y = x = y_max = y_min = x_max = x_min = 0;
 
         var move_cube = function(model) {
 
             the_cube = model;
 
             var interval_id_for_animation = setInterval(function() {
-                the_cube.position[selected_axis] = the_cube.position[selected_axis] + (2 * coeff_dir);
-            }, 1000);
+
+                if (is_changing) {
+                    return;
+                }
+
+                var z = the_cube.position.z;
+                x = the_cube.position.x;
+                y = the_cube.position.y;
+
+                y_max = parseFloat((y_max_coef * z).toFixed(1)) + 44;
+                y_min = parseFloat((y_min_coef * z).toFixed(1)) - 80;
+
+                x_max = parseFloat((x_max_coef * z).toFixed(1)) + 90;
+                x_min = parseFloat((x_min_coef * z).toFixed(1)) - 89;
+
+                console.log('x: ' + x + ', max: '+ x_max + ', min: ' + x_min);
+
+                if (selected_axis === 'z') {
+                    if (!(z_min < z && z < z_max)) {
+                        coeff_dir *= -1;
+                    }
+                } else if (selected_axis === 'y') {
+                    if (!(y_min < y && y < y_max)) {
+                        coeff_dir *= -1;
+                    }
+                } else if (selected_axis === 'x') {
+                    if (!(x_min < x && x < x_max)) {
+                        coeff_dir *= -1;
+                    }
+                } else {
+                    return;
+                }
+
+                the_cube.position[selected_axis] = the_cube.position[selected_axis] + (4 * coeff_dir);
+            }, 500);
         }
 
         var render_callback = function(scene) {
@@ -83,33 +127,55 @@
 
         var initControls = function() {
             $('#pgu-ny-cube-dir-left').off('click').on('click', function() {
-                selected_axis = 'x';
-                coeff_dir = -1;
+                is_changing = true;
+
+                if (x_min < x && x < x_max) {
+                    selected_axis = 'x';
+                    coeff_dir = -1;
+                }
+
+                is_changing = false;
             });
 
             $('#pgu-ny-cube-dir-right').off('click').on('click', function() {
-                selected_axis = 'x';
-                coeff_dir = 1;
+                is_changing = true;
+                if (x_min < x && x < x_max) {
+                    selected_axis = 'x';
+                    coeff_dir = 1;
+                }
+                is_changing = false;
             });
 
             $('#pgu-ny-cube-dir-up').off('click').on('click', function() {
-                selected_axis = 'y';
-                coeff_dir = 1;
+                is_changing = true;
+                if (y_min < y && y < y_max) {
+                    selected_axis = 'y';
+                    coeff_dir = 1;
+                }
+                is_changing = false;
             });
 
             $('#pgu-ny-cube-dir-down').off('click').on('click', function() {
-                selected_axis = 'y';
-                coeff_dir = -1;
+                is_changing = true;
+                if (y_min < y && y < y_max) {
+                    selected_axis = 'y';
+                    coeff_dir = -1;
+                }
+                is_changing = false;
             });
 
             $('#pgu-ny-cube-dir-fg').off('click').on('click', function() {
+                is_changing = true;
                 selected_axis = 'z';
                 coeff_dir = 1;
+                is_changing = false;
             });
 
             $('#pgu-ny-cube-dir-bg').off('click').on('click', function() {
+                is_changing = true;
                 selected_axis = 'z';
                 coeff_dir = -1;
+                is_changing = false;
             });
 
             //
