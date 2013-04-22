62f66098e641dbf9e4d39b68c5bde817b874f9fa
Pascal Guilcher <guilcher.p@sfeir.com>
2013-04-22 17:36:37 +0200

    ..

--- web/slides/pgu-london/pgu-london.js
+++ web/slides/pgu-london/pgu-london.js
@@ -179,24 +179,72 @@
         }
     }
 
-    function start_game() {
-        var screen = new TitleScreen('Sfeir Invaders', 'press "k" to start',
-            function() {
-                game.loadBoard(new GameBoard());
-            }
-        );
-        game.loadBoard(screen);
-        game.loop();
-    }
-
     var GameBoard = function()  {
+        var board = this;
+
+        this.objects = [];
+        this.cnt = {};
+
         this.removed_objs = [];
         this.missiles = 0;
 
-        var board = this;
 
-        this.add = function(obj) { obj.board = this; this.objects.push(obj); return obj;}
-        this.remove = function(obj) { this.removed_objs.push(obj); }
+        this.add = function(obj) {
+            obj.board=this;
+            this.objects.push(obj);
+            this.cnt[obj.type] = (this.cnt[obj.type] || 0) + 1;
+            return obj;
+        };
+
+        // Mark an object for removal
+        this.remove = function(obj) {
+            var idx = this.removed.indexOf(obj);
+            if(idx == -1) {
+                this.removed.push(obj);
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        // Reset the list of removed objects
+        this.resetRemoved = function() { this.removed = []; };
+
+        // Removed an objects marked for removal from the list
+        this.finalizeRemoved = function() {
+            for(var i=0,len=this.removed.length;i<len;i++) {
+                var idx = this.objects.indexOf(this.removed[i]);
+                if(idx != -1) {
+                    this.cnt[this.removed[i].type]--;
+                    this.objects.splice(idx,1);
+                }
+            }
+        };
+
+        // Call the same method on all current objects
+        this.iterate = function(funcName) {
+            var args = Array.prototype.slice.call(arguments,1);
+            for(var i=0,len=this.objects.length;i<len;i++) {
+                var obj = this.objects[i];
+                obj[funcName].apply(obj,args);
+            }
+        };
+
+        // Find the first object for which func is true
+        this.detect = function(func) {
+            for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
+                if(func.call(this.objects[i])) return this.objects[i];
+            }
+            return false;
+        };
+
+        // Call step on all objects and them delete
+        // any object that have been marked for removal
+        this.step = function(dt) {
+            this.resetRemoved();
+            this.iterate('step',dt);
+            this.finalizeRemoved();
+        };
 
         this.addPicture = function(name, x, y, opts) {
             var ref = pictures.map[name];
@@ -207,55 +255,29 @@
             picture.h = ref.h;
             return picture;
         }
-    }
 
-    this.iterate = function(fn) {
-        for (var i= 0, len=this.objects.length; i<len; i++) {
-            fn.call(this.objects[i]);
-        }
-    }
+        this.draw= function(ctx) {
+            this.iterate('draw',ctx);
+        };
 
-    this.detect = function(fn) {
-        for (var i= 0, val = null, len = this.objects.length; i< len; i++) {
-            if (fn.call(this.objects[i])) return this.objects[i];
-        }
-        return false;
-    }
+        // Check for a collision between the
+        // bounding rects of two objects
+        this.overlap = function(o1,o2) {
+            return !((o1.y+o1.h-1<o2.y) || (o1.y>o2.y+o2.h-1) ||
+                (o1.x+o1.w-1<o2.x) || (o1.x>o2.x+o2.w-1));
+        };
 
-    this.step = function(dt) {
-        this.removed_objs = [];
-        this.iterate(function() {
-            if (!this.step(dt)) this.die();
-        });
+        // Find the first object that collides with obj
+        // match against an optional type
+        this.collide = function(obj,type) {
+            return this.detect(function() {
+                if(obj != this) {
+                    var col = (!type || this.type & type) && board.overlap(obj,this);
+                    return col ? this : false;
+                }
+            });
+        };
 
-        for(var i = 0, len = this.removed_objs.length; i < len; i++) {
-            var idx = this.objects.indexOf(this.removed_objs[i]);
-            if(idx != -1) this.objects.splice(idx, 1);
-        }
-    }
-
-    this.render = function(canvas) {
-        canvas.clearRect(0, 0, game.width, game.height);
-        this.iterate(function() {
-            this.draw(canvas);
-        })
-    }
-
-    this.collision = function(o1, o2) {
-        return !(
-                 (o1.y + o1.h -1 < o2.y)
-              || (o1.y > o2.y + o2.h -1)
-              || (o1.x + o1.w -1 < o2.x)
-              || (o1.x > o2.x + o2.w - 1)
-            );
-    }
-
-    this.collide = function(obj) {
-        return this.detect(function() {
-            if(obj != this) {
-                return board.collision(obj, this) ? this: false;
-            }
-        })
     }
 
     var game_level = [
@@ -435,5 +457,16 @@
                 {dy: 100});
         }
     }
+
+    function start_game() {
+        var screen = new TitleScreen('Sfeir Invaders', 'press "k" to start',
+            function() {
+                game.loadBoard(new GameBoard());
+            }
+        );
+        game.loadBoard(screen);
+        game.loop();
+    }
+
 
 })();
