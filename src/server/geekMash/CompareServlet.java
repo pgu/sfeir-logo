532a086888eccd68cedb1dcf82fd1b149529552c
Pascal Guilcher <guilcher.p@sfeir.com>
2013-06-21 18:06:37 +0200

    ..

---
+++ src/server/geekMash/CompareServlet.java
@@ -0,0 +1,16 @@
+package server.geekMash;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CompareServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("{\"name\":\"toto\"}");
+    }
+
+}
