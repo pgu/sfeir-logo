67ec0c03d368fe2810db9b7c95bfdab957fcc0c6
Guilcher Pascal <pgu.dev@gmail.com>
2013-06-22 11:29:48 +0200

    ..

--- web/slides/pgu-berlin/pgu-berlin.js
+++ web/slides/pgu-berlin/pgu-berlin.js
@@ -1,7 +1,21 @@
 (function() {
 
+    var dom_of_article = [
+          '<div class="container_mash">'
+        , ' <div class="mash entity_a">'
+        , '  <img id="pic_a" class="pic_mash" />'
+        , '  <p id="txt_a" class="txt_mash" />'
+        , ' </div>'
+        , ' <div class="mash entity_b">'
+        , '  <img id="pic_b" class="pic_mash" />'
+        , '  <p id="txt_b" class="txt_mash" />'
+        , ' </div>'
+        , ' <div class="clearfix"></div>'
+        , '</div>'
+    ];
+
     var article = $('#pgu-berlin');
-    article.html('<div></div>');
+    article.html(dom_of_article.join(''));
     article.addClass('pgu-article pgu-article-black');
 
     window.SLIDES['pgu-berlin'] = {
@@ -25,6 +39,24 @@
                 console.log("data");
                 console.log(data);
             });
+
+            var entity_a = {
+                type: 'geek',
+                text: 'Networking is one letter from "Not working"',
+                pictureUrl: 'http://25.media.tumblr.com/f74d771d1fc640d6eb32d4f7242b4b71/tumblr_moca9xWmSv1rcufs7o1_1280.png'
+            }
+            var entity_b = {
+                type: 'geek',
+                text: 'Teamwork: Tackle life side by side.',
+                pictureUrl: 'http://24.media.tumblr.com/9eed7a0ca40b301569a67fe29e3d3cc6/tumblr_mnotrbV7Hz1rcufs7o1_1280.png'
+            }
+
+            $('#pic_a').attr('src', entity_a.pictureUrl);
+            $('#pic_b').attr('src', entity_b.pictureUrl);
+
+            $('#txt_a').text(entity_a.text);
+            $('#txt_b').text(entity_b.text);
+
         }
     };
 
